0a641da0b5a73eeabf5c5487a5c17a4198955b32
Zzzaikin <zaikin.pakedovo@gmail.com>
2023-06-11 14:04:22 +0300

    Добавил заглушки и ХАРДКОД

---
+++ Inflow_Frontend/client-app/src/mock/CustomerMock/ICustomer.ts
@@ -0,0 +1,12 @@
+interface ICustomer {
+    name: string,
+    age: number,
+    phoneNumber: string | null,
+    email: string | null,
+    address: string  | null,
+    telegram: string  | null,
+    vk: string  | null,
+    viber: string | null
+}
+
+export default ICustomer;
---
+++ Inflow_Frontend/client-app/src/mock/data.ts
@@ -0,0 +1,64 @@
+import ICustomer from "./CustomerMock/ICustomer";
+
+export const customersMockData: Array<ICustomer> = [
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id492441785",
+        viber: "89562348519"
+    },
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id490241785",
+        viber: "89562348511"
+    },
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id490241785",
+        viber: "89562348511"
+    },
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id490241785",
+        viber: "89562348511"
+    },
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id490241785",
+        viber: "89562348511"
+    },
+    {
+        name: "Заикин Дмитрий Владимирович",
+        age: 25,
+        phoneNumber: "89562348511",
+        email: "zaikin.dmitrit@example.ru",
+        address: "Россия, г. Курск, ул. Ленина 9, д. 3, кв. 4",
+        telegram: "89562348511",
+        vk: "https://vk.com/id490241785",
+        viber: "89562348511"
+    }
+];
